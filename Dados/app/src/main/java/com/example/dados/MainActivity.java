971e19d292434ac476f918b37359415272cf7643
castillo098 <losvideojuegos7268098@gmail.com>
2020-08-28 20:33:02 -0500

    Dados
    Se creo un proyect en clases

---
+++ Dados/app/src/main/java/com/example/dados/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.dados;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView imgResult ;
+    private Button btnLanzar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imgResult=(ImageView) findViewById(R.id.resultImagen);
+        btnLanzar =(Button) findViewById(R.id.buttonLanzar);
+        btnLanzar.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        if (btnLanzar==v) {
+            Lanzar();
+        }
+    }
+    private void Lanzar() {
+        Random r = new Random();
+        int random= r.nextInt(6 )+1;
+        switch (random){
+            case 1:
+                imgResult.setImageResource(R.drawable.dice_1);
+                break;
+            case 2:
+                imgResult.setImageResource(R.drawable.dice_2);
+                break;
+            case 3:
+                imgResult.setImageResource(R.drawable.dice_3);
+                break;
+            case 4:
+                imgResult.setImageResource(R.drawable.dice_4);
+                break;
+            case 5:
+                imgResult.setImageResource(R.drawable.dice_5);
+                break;
+            case 6:
+                imgResult.setImageResource(R.drawable.dice_6);
+                break;
+            default:
+                imgResult.setImageResource(R.drawable.empty_dice);
+                break;
+        }
+    }
+}
